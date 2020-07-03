8a34c58846bb1ba71430bfd8ddfc4afbd6bb3ebd
Zach <zjsievers@gmail.com>
2020-07-03 11:22:04 -0400

    created basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+// import Alert from "./components/Alert";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Courses from "./pages/Courses";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+// import Wrapper from "./components/Wrapper";
+// import Footer from "./components/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+function App() {
+  return (
+   
+        // <Alert />
+        <Navbar />
+         
+
+  
+  );
 }
 
 export default App;
