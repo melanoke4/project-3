f394c13492a8930a3df0f862c70dd832f2c67030
Zach <zjsievers@gmail.com>
2020-07-03 23:07:14 -0400

    creating dynamic navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,55 +1,53 @@
-import React from "react";
+// import { BrowserRouter as Router } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  Container,
+  Dropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
+  NavLink,
+} from "reactstrap";
 import "./style.css";
 
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container">
-        <a href="/" className="navbar-brand">
-          <img src="./images/logo.PNG" className="logo"></img>
-        </a>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="">
-                Enroll
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                href=""
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-              >
-                Courses
-              </a>
-              <div className="dropdown-menu">
-                <a href="" className="dropdown-item">
-                  HTML5
-                </a>
-                <a href="" className="dropdown-item">
-                  CSS3
-                </a>
-                <a href="" className="dropdown-item">
-                  JavaScript
-                </a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-}
+const NavbarMain = (props) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-export default Navbar;
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
+  return (
+    <Navbar expand="lg" className="navbar">
+      <Container>
+        <NavbarBrand>
+          <img src="./images/logo.PNG" className="logo" />
+        </NavbarBrand>
+        <Nav pills className="justify-content-end">
+        <NavItem>
+            <NavLink href="#">About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Register</NavLink>
+          </NavItem>
+          <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle nav caret>
+              Courses
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>HTML5</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>CSS3</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>JavaScript</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarMain;
