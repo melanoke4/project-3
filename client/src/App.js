f170ec4502e61642ee5662a1ab6b8c01e6d5b9bf
Zach <zjsievers@gmail.com>
2020-07-06 23:59:07 -0400

    adjusted app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 // import Alert from "./components/Alert";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 // import About from "./pages/About";
-// import Courses from "./pages/Courses";
+import Courses from "./pages/Courses";
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 // import Footer from "./components/Footer";
 
@@ -15,9 +14,9 @@ function App() {
   return (
    <Router>
      <div>
-      <Navbar />
       <Wrapper>
-        <Route exact path="/" component={ Home }/>
+        <Route exact path="/" component={ Courses }/>
+        
       </Wrapper>
      </div>
    </Router>
