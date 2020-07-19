9fb59050232ae4519310fb42fa52c1b8a424456a
Zach <zjsievers@gmail.com>
2020-07-19 16:10:33 -0400

    content css, html, js // created quiz components

---
+++ client/src/components/ContentCSS/index.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import "./style.css";
+
+const ContentCSS = () => {
+    return(
+        <div className="container-fluid">
+      <h1 className="display-1 text-center">CSS<span><img src="./images/css.png" id="header-css" /></span></h1>
+      <div className="row">
+        <div className="col-lg-9">
+          <div className="alert alert-info text-center" role="alert">
+            CSS(Cascading Style Sheets): <br />
+            CSS describes how HTML elements are to be displayed on screen.It can control the layout of multiple web pages all at once.
+          </div>
+          <div className="row">
+
+            {/* Simple Ex begins */}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title"> Coolest Website Ever!</h5>
+                  <li className="card-text">
+                    <code> h1 </code> Large Header // You can set the font-size, text color, text alignment and background color
+                  </li>
+                  <li className="card-text">
+                    <code> h2 </code> Smaller Header
+                  </li>
+                  <li className="card-text">
+                    <code> h3 </code> Even Smaller Header
+                  </li>
+                  <li className="card-text">
+                    <code> p </code> Paragraph text - "Lorem" is a quick way to add text to your page.
+                  </li>
+                  <li className="card-text">
+                    <code> img </code> Center a block element in the middle of its enclosing - We'll explain Block Display in more detail in later sections.
+                  </li>
+                  <li className="card-text">
+                    <code> ul </code> unordered list // text-align: center ; 
+                  </li>
+                </div>
+              </div>
+            </div>
+            {/* Simple Ex Ends */}
+
+            {/* CSS Example */}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src="./images/css-example.PNG"
+                  id="example-html"
+                  alt="Card image cap"
+                />
+              </div>
+            </div>
+            {/* css Example ends*/}
+
+            {/* css code*/}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src="./images/css-code.PNG"
+                  id="example-html"
+                  alt="Card image cap"
+                />
+              </div>
+            </div>
+            {/* css code */}
+
+            {/* html code  */}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src="./images/css-html.PNG"
+                  id="example-html"
+                  alt="Card image cap"
+                />
+              </div>
+            </div>
+            {/* html code ends */}
+
+          </div>
+        </div>
+
+        {/* table section begins */}
+        <div className="col-lg-3">
+          <h5 className="text-center"> CSS Properties to Remember </h5>
+          <table>
+            
+            <tr>
+              <td>text-align</td>
+              <td>Specifies the horizontal alignment of text</td>
+            </tr>
+            <tr>
+              <td>padding</td>
+              <td>Adds padding to all sides of a element</td>
+            </tr>
+            <tr>
+              <td>height</td>
+              <td>Sets height of an element</td>
+            </tr>
+            <tr>
+              <td>font-size</td>
+              <td>Specifies the font size of text</td>
+            </tr>
+            <tr>
+              <td>color</td>
+              <td>Sets the color of text</td>
+            </tr>
+            <tr>
+              <td> # </td>
+              <td>Selector: targets an id element</td>
+            </tr>
+            <tr>
+              <td> p </td>
+              <td>Targets all p tags </td>
+            </tr>
+            <tr>
+              <td> . </td>
+              <td> Selector: targets an class element </td>
+            </tr>
+            <tr>
+              <td> background-color </td>
+              <td>Specifies the background color of an element</td>
+            </tr>
+          </table>
+        </div>
+        {/* table section ends */}
+
+      </div>
+    </div>
+    );
+};
+
+export default ContentCSS;
---
+++ client/src/components/ContentHTML/index.js
@@ -0,0 +1,178 @@
+import React from "react";
+import "./style.css";
+
+const ContentHTML = () => {
+  return (
+    <div className="container-fluid">
+      <h1 className="display-1 text-center">HTML<span><img src="./images/html.png" id="header-html" /> </span></h1>
+      <div className="row">
+        <div className="col-lg-9">
+          <div className="alert alert-info text-center" role="alert">
+            HTML(Hyper Text Markup Language): <br />
+            The standard markup language consisting of a series of elements that
+            tell the browser how to display the content to the page.
+          </div>
+          <div className="row">
+
+            {/* Simple Ex begins */}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title"> Simple HTML Document </h5>
+                  <li className="card-text">
+                    <code> !DOCTYPE </code> Defines that htis document is an
+                    HTML5 doc
+                  </li>
+                  <li className="card-text">
+                    <code> html </code> Root element of an HTML page
+                  </li>
+                  <li className="card-text">
+                    <code> head </code> Contains meta information about the HTML
+                    page
+                  </li>
+                  <li className="card-text">
+                    <code> title </code> Specifies a title for the HTML page
+                  </li>
+                  <li className="card-text">
+                    <code> h1 </code> Defines a large heading
+                  </li>
+                  <li className="card-text">
+                    <code> p </code> Defines a paragraph
+                  </li>
+                  <li className="card-text">
+                    <code> body </code> Defines the document's body. A container
+                    for contents, such as headings, paragraphs, images and
+                    links.
+                  </li>
+                </div>
+              </div>
+            </div>
+            {/* Simple Ex Ends */}
+
+            {/* Simple Example Image */}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src="./images/example-html.PNG"
+                  id="example-html"
+                  alt="Card image cap"
+                />
+              </div>
+            </div>
+            {/* Simple Example ends*/}
+
+            {/* Intermediate Example Begins */}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title"> Intermediate HTML Document </h5>
+                  <li className="card-text">
+                    <code> !DOCTYPE </code> Defines that htis document is an
+                    HTML5 doc
+                  </li>
+                  <li className="card-text">
+                    <code> html </code> Root element of an HTML page
+                  </li>
+                  <li className="card-text">
+                    <code> head </code> Contains meta information about the HTML
+                    page
+                  </li>
+                  <li className="card-text">
+                    <code> title </code> Specifies a title for the HTML page
+                  </li>
+                  <li className="card-text">
+                    <code> h1 </code> Defines a large heading
+                  </li>
+                  <li className="card-text">
+                    <code> p </code> Defines a paragraph
+                  </li>
+                  <li className="card-text">
+                    <code> body </code> Defines the document's body. A container
+                    for contents, such as headings, paragraphs, images and
+                    links.
+                  </li>
+                </div>
+              </div>
+            </div>
+            {/* Intermediate Example Ends */}
+
+            {/* example card */}
+            <div className="col-xl-6 col-sm-6 mt-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src="./images/example-html.PNG"
+                  id="example-html"
+                  alt="Card image cap"
+                />
+              </div>
+            </div>
+            {/* example card ends */}
+
+          </div>
+        </div>
+
+        {/* table section begins */}
+        <div className="col-lg-3">
+          <h5 className="text-center"> HTML Tags to Remember </h5>
+          <table>
+            
+            <tr>
+              <td> !DOCTYPE </td>
+              <td> Defines the document type </td>
+            </tr>
+            <tr>
+              <td> b </td>
+              <td> Bolds the text </td>
+            </tr>
+            <tr>
+              <td> br </td>
+              <td> Creates a line break </td>
+            </tr>
+            <tr>
+              <td> img </td>
+              <td> Image </td>
+            </tr>
+            <tr>
+              <td> li </td>
+              <td> List Item </td>
+            </tr>
+            <tr>
+              <td> p </td>
+              <td> Paragraph </td>
+            </tr>
+            <tr>
+              <td> a </td>
+              <td> Link </td>
+            </tr>
+            <tr>
+              <td> div </td>
+              <td> Creates a section in the document </td>
+            </tr>
+            <tr>
+              <td> head </td>
+              <td> Contains information for the document </td>
+            </tr>
+            <tr>
+              <td> table </td>
+              <td> Defines a table </td>
+            </tr>
+            <tr>
+              <td> title </td>
+              <td> Defines a title for the document </td>
+            </tr>
+            <tr>
+              <td> button </td>
+              <td> Defines a clickable button </td>
+            </tr>
+          </table>
+        </div>
+        {/* table section ends */}
+
+      </div>
+    </div>
+  );
+};
+
+export default ContentHTML;
