9abc3f7a65135ada70bd06fff3b14574498a91b2
Vance Newberry <vancenewberry@gmail.com>
2020-07-19 17:23:42 -0500

    resolved conflict

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
   function Question(props) {
     return (
-<<<<<<< HEAD:client/src/components/Question/index.js
-      <h2 className="question">{props.question}</h2>
-=======
       <h1>{props.question}</h1>
->>>>>>> 9a2a3ecbc48c8304eabda675d39a40de13741150:client/src/components/Question.jsx
     );
   }
  
