1fbafde4e3c227e45404bc59f72f1987296b0c6e
Vance Newberry <vancenewberry@gmail.com>
2020-07-20 12:18:47 -0500

    created profile controller and edited user model

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
  
-const User = new Schema({});
+const User = new Schema({
+    premiumUser: {
+        type: Boolean,
+        default: false,
+        userCreated: {
+            type: Date,
+            default: Date.now
+          },
+    }
+});
  
 User.plugin(passportLocalMongoose);
  
