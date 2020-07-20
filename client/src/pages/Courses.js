1e309d142a48ebeb0196b8ed662d4a7e524d9beb
Vance Newberry <vancenewberry@gmail.com>
2020-07-20 09:44:56 -0500

    removed extra stripe button and added avatar

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -3,7 +3,7 @@ import Alert from "../components/Alert";
 import Navbar from "../components/Navbar";
 import CourseGroup from "../components/CourseGroup";
 import Quotes from "../components/Quotes";
-import Checkout from "../Checkout";
+
 
 
 
@@ -13,13 +13,6 @@ function Courses() {
             <Alert />
             <Navbar />
             <CourseGroup />
-            <p className="App-intro">
-          <Checkout
-            name={'Your Company Name'}
-            description={'Item that you sold'}
-            amount={4.99}
-          />
-        </p>
             <Quotes />
         </div>
     );
