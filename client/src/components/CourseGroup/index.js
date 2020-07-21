1f6db6128b8c3ffcb2098c60a674c5af204d3db5
Zach <zjsievers@gmail.com>
2020-07-21 13:59:34 -0400

    spacing / styling content button

--- client/src/components/CourseGroup/index.js
+++ client/src/components/CourseGroup/index.js
@@ -28,18 +28,18 @@ const CoursesSection = (props) => {
                   <div className="card-body">
                     <h5 className="card-title">{product.name}</h5>
                     {/* this is stripe button  */}
-                    <p className="App-intro" id="purchase">
-                      <Checkout
-                        name={"Kids Coding Corner"}
-                        description={product.name}
-                        amount={product.cost}
-                      />
-                    </p>              
-{/* ===================================================== */}
-                    <a href={product.link} className="btn btn-outline-primary btn-sm" id="content-btn">
-                      Course Content
-                    </a>
-                                        
+                      <p className="App-intro" id="purchase">
+                        <Checkout
+                          name={"Kids Coding Corner"}
+                          description={product.name}
+                          amount={product.cost}
+                        />
+                      </p>              
+                      {/* ===================================================== */}
+                      <a href={product.link} className="btn btn-sm" id="content-btn">
+                        Course Content
+                      </a>
+                                    
                     <h6 className="card-subtitle text-muted mb-2">
                       ${product.cost}
                     </h6>
