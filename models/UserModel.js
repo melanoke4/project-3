ac6cfaf267138fc896e8711672495992aa8cde7c
Vance Newberry <vancenewberry@gmail.com>
2020-07-20 20:07:15 -0500

    added a footer to attribute icons creator, work on user model and profile updater

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
- 
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const User = new Schema({
-    premiumUser: {
-        type: Boolean,
-        default: false,
-        userCreated: {
-            type: Date,
-            default: Date.now
-          },
-    }
+  premiumUser: {
+    type: Boolean,
+    default: false,
+  },
+  userCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  profilePicture: String,
 });
- 
+
 User.plugin(passportLocalMongoose);
- 
-module.exports = mongoose.model('User', User);
+
+module.exports = mongoose.model("User", User);
